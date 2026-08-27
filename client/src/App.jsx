aca3bdab81c6e3c2dfdc686dffc6f1aff0ebf4e5
toffly <toffly@outlook.com>
2026-08-27 11:40:15 +0700

    Add routing and create Home and Blog components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,15 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Blog from "./pages/Blog"
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/blog" element={<Blog/>}/>
+      </Routes>
+    </div>
   )
 }
 
