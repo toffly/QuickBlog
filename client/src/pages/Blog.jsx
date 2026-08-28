ed45bf7a60d3552b2679446f68bbb28b6d5fefc9
toffly <toffly@outlook.com>
2026-08-28 12:46:34 +0700

    Add Loader component for improved loading state in Blog component

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -4,6 +4,7 @@ import { assets, blog_data, comments_data } from "../assets/assets";
 import Navbar from "../components/Navbar";
 import Moment from "moment";
 import Footer from "../components/Footer";
+import Loader from "../components/Loader";
 
 const Blog = () => {
   const { id } = useParams();
@@ -124,7 +125,7 @@ const Blog = () => {
       <Footer/>
     </div>
   ) : (
-    <div>Loading...</div>
+    <Loader/>
   );
 };
 
