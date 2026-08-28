ca06e9cdb1a478834a8c7fdd875af2f88ba1e545
toffly <toffly@outlook.com>
2026-08-28 11:54:10 +0700

    Enhance Blog component layout and styling; add rich text support for blog description

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -30,12 +30,24 @@ const Blog = () => {
       />
       <Navbar />
       <div className="text-center mt-20 text-gray-600">
-        <p className="text-primary py-4 font-medium">Published on {Moment(data.createdAt).format("MMMM Do YYYY")}</p>
-        <h1 className="text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto">{data.title}</h1>
+        <p className="text-primary py-4 font-medium">
+          Published on {Moment(data.createdAt).format("MMMM Do YYYY")}
+        </p>
+        <h1 className="text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto">
+          {data.title}
+        </h1>
         <h1 className="my-5 max-w-lg truncate mx-auto">{data.subTitle}</h1>
-        <p className="inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-primary">Michael Brown</p>
+        <p className="inline-block py-1 px-4 rounded-full mb-6 border text-sm border-primary/35 bg-primary/5 font-medium text-primary">
+          Michael Brown
+        </p>
       </div>
-      <div></div>
+      <div className="mx-5 max-w-5xl md:mx-auto my-10 mt-6">
+        <img src={data.image} alt="Image" className="rounded-3xl mb-5" />
+        <div
+          dangerouslySetInnerHTML={{ __html: data.description }}
+          className="rich-text max-w-3xl mx-auto"
+        ></div>
+      </div>
     </div>
   ) : (
     <div>Loading...</div>
