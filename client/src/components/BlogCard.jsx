34c0fc333e6754ad97d64dbaade42233a043d76f
toffly <toffly@outlook.com>
2026-08-28 11:14:44 +0700

    Add NewsLetter component and integrate it into Home page; update BlogCard to render HTML safely and fix Blog route

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -12,7 +12,7 @@ const BlogCard = ({ blog }) => {
       </span>
       <div className="p-5">
         <h5 className="mb-2 font-medium text-gray-900">{title}</h5>
-        <p className="mb-3 text-xs text-gray-600">{description.slice(0,80)}</p>
+        <p className="mb-3 text-xs text-gray-600" dangerouslySetInnerHTML={{"__html":description.slice(0,80)}}></p>
       </div>
     </div>
   );
