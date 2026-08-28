f375b198bffba733f12e23cb2a8228241b679447
toffly <toffly@outlook.com>
2026-08-28 11:24:38 +0700

    Add Footer component and integrate it into Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import BlogList from "../components/BlogList"
+import Footer from "../components/Footer"
 import Header from "../components/Header"
 import Navbar from "../components/Navbar"
 import NewsLetter from "../components/NewsLetter"
@@ -10,6 +11,7 @@ const Home = () => {
         <Header/>
         <BlogList/>
         <NewsLetter/>
+        <Footer/>
     </>
   )
 }
