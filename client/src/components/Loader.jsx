985b7033b509346f0560a892bb724251f675abd5
toffly <toffly@outlook.com>
2026-08-28 12:46:34 +0700

    Add Loader component for improved loading state in Blog component

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,7 @@
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center h-screen"><div className="animate-spin rounded-full h-16 w-16 border-4 border-t-white border-gray-700F"></div></div>
+  )
+}
+
+export default Loader
