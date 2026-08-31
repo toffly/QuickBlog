aff674996033eb213836d45f282ac4400816da7f
toffly <toffly@outlook.com>
2026-08-31 12:06:11 +0700

    Implement ListBlog component to display all blogs with a table layout

--- client/src/pages/admin/ListBlog.jsx
+++ client/src/pages/admin/ListBlog.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { blog_data } from "../../assets/assets";
+import BlogTableItem from "../../components/admin/BlogTableItem";
 
 const ListBlog = () => {
-  return (
-    <div>ListBlog</div>
-  )
-}
+  const [blogs, setBlogs] = useState([]);
 
-export default ListBlog
+  const fetchBlogs = async () => {
+    setBlogs(blog_data);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetchBlogs();
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="flex-1 pt-5 px-5 sm:pt-12 sm:pl-16 bg-blue-50/50">
+      <h1>All Blogs</h1>
+
+      <div className="relative h-4/5 max-w-4xl overflow-x-auto shadow rounded-xl scrollbar-hide bg-white mt-4">
+          <table className="w-full text-sm text-gray-500">
+            <thead className="text-xs text-gray-600 text-left uppercase">
+              <tr>
+                <th scope="col" className="px-2 py-4 xl:px-6">
+                  #
+                </th>
+                <th scope="col" className="px-2 py-4">
+                  Blog Title
+                </th>
+                <th scope="col" className="px-2 py-4 max-sm:hidden">
+                  Date
+                </th>
+                <th scope="col" className="px-2 py-4 max-sm:hidden">
+                  Status
+                </th>
+                <th scope="col" className="px-2 py-4">
+                  Actions
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {blogs.map((blog, index) => {
+                return (
+                  <BlogTableItem
+                    key={blog._id}
+                    blog={blog}
+                    fetchBlogs={fetchBlogs}
+                    index={index + 1}
+                  />
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+    </div>
+  );
+};
+
+export default ListBlog;
