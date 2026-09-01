0ceb7e316a75f89447a58b6fd8934bc7dd9250a2
toffly <toffly@outlook.com>
2026-09-01 15:01:23 +0700

    Add database connection setup and environment configuration

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import connectDB from "./configs/db.js";
+
+import dns from "node:dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const app = express();
+
+await connectDB()
 
 app.use(cors());
 app.use(express.json());
