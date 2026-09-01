e4d78b60601cee204f0a5b89dabc7b2c40a6e021
toffly <toffly@outlook.com>
2026-09-01 14:43:41 +0700

    Set up server with Express and CORS; add .env file for environment variables

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("API is working"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT)
+})
+
+export default app
