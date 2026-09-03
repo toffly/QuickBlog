26392b614c14eb9d37afaddd03bac41bda2ee078
toffly <toffly@outlook.com>
2026-09-03 14:41:26 +0700

    fix: correct import name for Newsletter component in Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import BlogList from "../components/BlogList"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 import Navbar from "../components/Navbar"
-import NewsLetter from "../components/NewsLetter"
+import Newsletter from "../components/Newsletter"
 
 const Home = () => {
   return (
@@ -10,7 +10,7 @@ const Home = () => {
         <Navbar/>
         <Header/>
         <BlogList/>
-        <NewsLetter/>
+        <Newsletter/>
         <Footer/>
     </>
   )
