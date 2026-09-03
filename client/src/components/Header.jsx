98dfcd8ff495fb851f15a379a8da89995d1e3390
toffly <toffly@outlook.com>
2026-09-03 12:28:25 +0700

    feat: enhance BlogList filtering, update Header for search input handling, and fix Newsletter heading tag

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,21 @@
+import { useRef } from "react";
 import { assets } from "../assets/assets";
+import { useAppContext } from "../context/AppContext";
 
 const Header = () => {
+  const inputRef = useRef();
+  const { setInput, input } = useAppContext();
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    setInput(inputRef.current.value);
+  };
+
+  const onClear = () => {
+    setInput("");
+    inputRef.current.value = "";
+  };
+
   return (
     <div className="mx-8 sm:mx-16 xl:mx-24 relative">
       <div className="text-center mt-20 mb-8">
@@ -19,8 +34,12 @@ const Header = () => {
           starts right here.
         </p>
 
-        <form className="flex justify-between max-w-lg max-sm:scale-75 mx-auto border-gray-300 bg-white rounded overflow-hidden">
+        <form
+          onSubmit={onSubmitHandler}
+          className="flex justify-between max-w-lg max-sm:scale-75 mx-auto border-gray-300 bg-white rounded overflow-hidden"
+        >
           <input
+            ref={inputRef}
             type="text"
             placeholder="Search for blogs"
             required
@@ -34,6 +53,18 @@ const Header = () => {
           </button>
         </form>
       </div>
+
+      <div className="text-center">
+        {input && (
+          <button
+            onClick={onClear}
+            className="border font-light text-xs py-1 px-3 rounded-sm shadow-custom-sm cursor-pointer"
+          >
+            Clear Search
+          </button>
+        )}
+      </div>
+
       <img
         src={assets.gradientBackground}
         alt="bg"
