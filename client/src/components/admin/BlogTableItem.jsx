1fb5a282ffc2ea5fee0af8750803d7c08876e00a
toffly <toffly@outlook.com>
2026-09-03 13:12:35 +0700

    feat: enhance blog management with delete and publish toggle functionality, improve AddBlog form handling, and update UI elements

--- client/src/components/admin/BlogTableItem.jsx
+++ client/src/components/admin/BlogTableItem.jsx
@@ -1,8 +1,45 @@
+import toast from "react-hot-toast";
 import { assets } from "../../assets/assets";
+import { useAppContext } from "../../context/AppContext";
 
 const BlogTableItem = ({ blog, fetchBlogs, index }) => {
+  const { axios } = useAppContext();
   const { title, createdAt } = blog;
   const BlogData = new Date(createdAt);
+
+  const deleteBlog = async () => {
+    const confirm = window.confirm(
+      "Are you sure you want to delete this blog?",
+    );
+    if (!confirm) return;
+    try {
+      const { data } = await axios.post("/api/blog/delete", { id: blog._id });
+      if (data.success) {
+        toast.success(data.message);
+        await fetchBlogs();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const togglePublish = async () => {
+    try {
+      const { data } = await axios.post("/api/blog/toggle-publish", {
+        id: blog._id,
+      });
+      if (data.success) {
+        toast.success(data.message);
+        await fetchBlogs();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
   return (
     <tr className="border-y border-gray-300">
@@ -17,10 +54,14 @@ const BlogTableItem = ({ blog, fetchBlogs, index }) => {
         </p>
       </td>
       <td className="px-2  py-4 flex text-xs gap-3">
-        <button className="border px-2 py-0.5 mt-1 rounded cursor-pointer">
+        <button
+          onClick={togglePublish}
+          className="border px-2 py-0.5 mt-1 rounded cursor-pointer"
+        >
           {blog.isPublished ? "Unpublished" : "Published"}
         </button>
         <img
+          onClick={deleteBlog}
           src={assets.cross_icon}
           alt="buttonIcon"
           className="w-8 overflow-auto scale-110 transition-all cursor-pointer"
--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -1,10 +1,15 @@
 import { useEffect, useRef, useState } from "react";
 import { assets, blogCategories } from "../../assets/assets";
 import Quill from "quill";
+import { useAppContext } from "../../context/AppContext";
+import toast from "react-hot-toast";
 
 const AddBlog = () => {
+  const { axios } = useAppContext();
+  const [isAdding, setIsAdding] = useState(false);
+
   const editorRef = useRef(null);
-  const Quillref = useRef(null);
+  const quillref = useRef(null);
 
   const [image, setImage] = useState(false);
   const [title, setTitle] = useState("");
@@ -13,14 +18,43 @@ const AddBlog = () => {
   const [isPublished, setIsPublished] = useState(false);
 
   const onSubmithandler = async (e) => {
-    e.preventDefault();
+    try {
+      e.preventDefault();
+      setIsAdding(true);
+
+      const blog = {
+        title,
+        subTitle,
+        description: quillref.current.root.innerHTML,
+        category,
+        isPublished,
+      };
+      const formData = new FormData();
+      formData.append("blog", JSON.stringify(blog));
+      formData.append("image", image);
+
+      const { data } = await axios.post("/api/blog/add", formData);
+      if (data.success) {
+        toast.success(data.message);
+        setImage(false);
+        setTitle("");
+        quillref.current.root.innerHTML = "";
+        setCategory("Startup");
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setIsAdding(false);
+    }
   };
 
   const generateContent = async () => {};
 
   useEffect(() => {
-    if (!Quillref.current && editorRef.current) {
-      Quillref.current = new Quill(editorRef.current, { theme: "snow" });
+    if (!quillref.current && editorRef.current) {
+      quillref.current = new Quill(editorRef.current, { theme: "snow" });
     }
   }, []);
 
@@ -102,10 +136,11 @@ const AddBlog = () => {
         </div>
 
         <button
+          disabled={isAdding}
           type="Submit"
           className="mt-8 w-40 h-10 bg-primary text-white rounded cursor-pointer text-sm"
         >
-          Add Blog
+          {isAdding ? "Adding..." : "Add Blog"}
         </button>
       </div>
     </form>
